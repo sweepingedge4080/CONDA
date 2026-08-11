b323e5f1b7c7605a9479777dd6691edfb0e35e1f
sweepingedge4080 <yujigraphics@gmail.com>
2026-08-11 17:23:32 +0300

    Update disconnectHandler.js

--- modules/disconnectHandler.js
+++ modules/disconnectHandler.js
@@ -1,31 +1,41 @@
 // Feature: Handle user disconnection
 const { leaveRoom, rooms } = require('./roomManager');
-const { removeUser, updateUserRoom } = require('./userManager');
+const { removeUser } = require('./userManager');
 
 function handleDisconnect(socket, io) {
   socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id);
+    console.log('❌ Client disconnected:', socket.id);
     
     // Find and leave all rooms this user is in
+    let disconnectedRooms = [];
     for (const [roomId, room] of rooms.entries()) {
       if (room.users.has(socket.id)) {
         leaveRoom(roomId, socket.id);
         socket.leave(roomId);
+        disconnectedRooms.push(roomId);
         
-        // Notify remaining users in the room
-        io.to(roomId).emit('partner-disconnected', {
-          message: 'Your partner has disconnected'
-        });
-        
-        // If room is empty, clean it up
-        if (room.users.size === 0) {
-          rooms.delete(roomId);
+        // Notify remaining users
+        if (room.users.size > 0) {
+          io.to(roomId).emit('partner-disconnected', {
+            message: 'Your partner has disconnected'
+          });
+          console.log(`📢 Notified remaining users in room ${roomId}`);
         }
+      }
+    }
+    
+    // Clean up empty rooms
+    for (const roomId of disconnectedRooms) {
+      const room = rooms.get(roomId);
+      if (room && room.users.size === 0) {
+        rooms.delete(roomId);
+        console.log(`🗑️ Removed empty room: ${roomId}`);
       }
     }
     
     // Remove user from tracking
     removeUser(socket.id);
+    console.log(`👤 Removed user ${socket.id} from tracking`);
   });
 }
 
